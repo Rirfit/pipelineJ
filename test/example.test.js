405e2201d9e66a1bc717a0debf1e4b3073325c11
Josec690 <joseclaudiley@gmail.com>
2025-05-24 09:23:56 -0300

    test actions 3

--- test/example.test.js
+++ test/example.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 describe('Exemplo de Teste', function(){
     it('deve somar dois numeros corretamente', function(){
-        assert.strictEqual(1+1, 2);
+        assert.strictEqual(1+3, 2);
     });
 });
